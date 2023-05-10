b0d39824a2c44380b17578834d62d9b1ccc4634a
George Miller <george.miller0042@gmail.com>
2023-05-10 14:11:16 +1000

    auto catch up the unprocessed interest payment

--- x/spv/abci.go
+++ x/spv/abci.go
@@ -11,39 +11,47 @@ import (
 func EndBlock(ctx sdk.Context, k keeper.Keeper) {
 	currentTime := ctx.BlockTime()
 	// we firstly handle the interest
-
+	c := true
 	k.IteratePool(ctx, func(poolInfo types.PoolInfo) (stop bool) {
-		dueTime := poolInfo.LastPaymentTime.Add(time.Second * time.Duration(poolInfo.PayFreq))
-		if dueTime.Before(currentTime) {
-			if poolInfo.PoolStatus == types.PoolInfo_ACTIVE || poolInfo.PoolStatus == types.PoolInfo_FREEZING || poolInfo.PoolStatus == types.PoolInfo_PooLPayPartially {
+		for c {
+			dueTime := poolInfo.LastPaymentTime.Add(time.Second * time.Duration(poolInfo.PayFreq))
+			poolReady := poolInfo.PoolStatus == types.PoolInfo_ACTIVE || poolInfo.PoolStatus == types.PoolInfo_FREEZING || poolInfo.PoolStatus == types.PoolInfo_PooLPayPartially
+			if dueTime.Before(currentTime) && poolReady {
 				err := k.HandleInterest(ctx, &poolInfo)
 				if err != nil {
-					ctx.Logger().Error(err.Error())
-					return false
-				}
-			}
-			if poolInfo.PoolStatus == types.PoolInfo_ACTIVE || poolInfo.PoolStatus == types.PoolInfo_PooLPayPartially {
-				processed := k.HandleTransfer(ctx, &poolInfo)
-				if processed {
-					k.SetPool(ctx, poolInfo)
-					return false
-				}
-				if poolInfo.ProjectDueTime.Before(currentTime) {
-					// we pay the partial of the interest
-					if poolInfo.PoolStatus == types.PoolInfo_PooLPayPartially {
-						k.HandlePartialPrincipalPayment(ctx, &poolInfo, poolInfo.GetWithdrawAccounts())
+					if err.Error() == "pay interest too early" {
+						c = false
 					}
-					// we update the project due time to the next cycle
-					poolInfo.ProjectDueTime = poolInfo.ProjectDueTime.Add(time.Second * time.Duration(poolInfo.ProjectLength))
+					panic(err)
 				}
+				ctx.Logger().Info("process interest", "pool Index:", poolInfo.Index, "latest payment", poolInfo.LastPaymentTime.Local().String())
+			} else {
+				c = false
+			}
+		}
+
+		if poolInfo.PoolStatus == types.PoolInfo_ACTIVE || poolInfo.PoolStatus == types.PoolInfo_PooLPayPartially {
+			processed := k.HandleTransfer(ctx, &poolInfo)
+			if processed {
 				k.SetPool(ctx, poolInfo)
 				return false
 			}
-			if poolInfo.PoolStatus == types.PoolInfo_FREEZING {
-				k.HandlePrincipalPayment(ctx, &poolInfo)
+			ctx.Logger().Info("pool due time update", "index", poolInfo.Index, "due time", poolInfo.ProjectDueTime.Local().String())
+			if poolInfo.ProjectDueTime.Before(currentTime) {
+				// we pay the partial of the interest
+				if poolInfo.PoolStatus == types.PoolInfo_PooLPayPartially {
+					k.HandlePartialPrincipalPayment(ctx, &poolInfo, poolInfo.GetWithdrawAccounts())
+				}
+				// we update the project due time to the next cycle
+				poolInfo.ProjectDueTime = poolInfo.ProjectDueTime.Add(time.Second * time.Duration(poolInfo.ProjectLength))
 			}
 			k.SetPool(ctx, poolInfo)
+			return false
 		}
+		if poolInfo.PoolStatus == types.PoolInfo_FREEZING {
+			k.HandlePrincipalPayment(ctx, &poolInfo)
+		}
+		k.SetPool(ctx, poolInfo)
 
 		return false
 	})
