c5cb1801395761e4e79d2191f22fdedae285b146
george.miller <george.miller00404@gmail.com>
2024-08-16 16:53:00 +1000

    before update app.go

--- testutil/dydx/testutil/keeper/blocktime.go
+++ testutil/dydx/testutil/keeper/blocktime.go
@@ -1,9 +1,13 @@
 package keeper
 
 import (
+	"testing"
+
 	storetypes "cosmossdk.io/store/types"
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/joltify-finance/joltify_lending/lib"
 	"github.com/joltify-finance/joltify_lending/x/third_party_dydx/blocktime/keeper"
 	"github.com/joltify-finance/joltify_lending/x/third_party_dydx/blocktime/types"
@@ -29,4 +33,26 @@ func createBlockTimeKeeper(
 	)
 
 	return k, storeKey
+}
+
+func BlcokTimeKeepers(
+	t testing.TB,
+) (
+	ctx sdk.Context,
+	keeper *keeper.Keeper,
+	storeKey storetypes.StoreKey,
+) {
+	ctx = initKeepers(t, func(
+		db *dbm.MemDB,
+		registry codectypes.InterfaceRegistry,
+		cdc *codec.ProtoCodec,
+		stateStore storetypes.CommitMultiStore,
+		transientStoreKey storetypes.StoreKey,
+	) []GenesisInitializer {
+		// Define necessary keepers here for unit tests
+		keeper, storeKey = createBlockTimeKeeper(stateStore, db, cdc)
+
+		return []GenesisInitializer{keeper}
+	})
+	return ctx, keeper, storeKey
 }
